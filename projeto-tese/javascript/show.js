3fab5e0432298d20ea4c9c9b914c3726d7861d32
HelenaSabel <helena.b.sabel@gmail.com>
2016-03-03 16:46:49 -0500

    working on the form

--- projeto-tese/javascript/show.js
+++ projeto-tese/javascript/show.js
@@ -1,20 +1,42 @@
-function init() {
-    var inputs = document.getElementsByClassName('show');
-    for (var i = 0; i <inputs.length; i++) {
-        inputs[i].addEventListener('click', show, false);
-    }
+/*function init() {
+var inputs = document.getElementsByClassName('show');
+for (var i = 0; i <inputs.length; i++) {
+inputs[i].addEventListener('click', show, false);
+}
 }
 
 function show(){
-    this.nextElementSibling.nextElementSibling.nextElementSibling.classList.toggle('hide');
-    var uls = document.getElementsByElementName('ul');
-    for (var i = 0; i <uls.length; i++) {
-        uls[i].previousElementSibling.previousElementSibling.previousElementSibling.addEventListener('click', destroy, false);
-    }
+this.nextElementSibling.nextElementSibling.nextElementSibling.classList.toggle('hide');
+var uls = document.getElementsByElementName('ul');
+for (var i = 0; i <uls.length; i++) {
+uls[i].previousElementSibling.previousElementSibling.previousElementSibling.addEventListener('click', destroy, false);
+}
 }
 
 function destroy() {
-     this.nextElementSibling.nextElementSibling.nextElementSibling.classList.toggle('hide');
+this.nextElementSibling.nextElementSibling.nextElementSibling.classList.toggle('hide');
 }
+
+window.onload = init*/
+
+
+
+function init() {
+    var inputs = document.getElementsByClassName('show');
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].addEventListener('change', show, true);
+    }
+}
+
+function show() {
+    if (this.checked) {
+        this.nextElementSibling.nextElementSibling.nextElementSibling.classList.remove('hide');
+    } else {
+        this.nextElementSibling.nextElementSibling.nextElementSibling.classList.add('hide');
+    }
+
+}
+
+
 
 window.onload = init
